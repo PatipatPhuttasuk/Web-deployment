eef64af7f2c3835330c9283d2e4f4ca9725252da
PatipatPhuttasuk <5835512104@psu.ac.th>
2020-04-19 13:21:51 +0700

    Update index.js

--- pm2/index.js
+++ pm2/index.js
@@ -2,8 +2,15 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => {
-    console.log('Hello world')
-    res.send('Hello world')
+    console.log('Web deployment')
+    res.send({
+
+        Checkpoint: '9.1',
+        ID: 5835512104,
+        Name: 'Patipat',
+        Surname: 'Phuttasuk',
+        Sec: 01
+    })
 })
 
 app.get('/crash', (req, res) => {
